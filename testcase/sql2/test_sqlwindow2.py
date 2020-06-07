9fcc93535bcc097a1eb6c0de3892ec0c03b3359d
legend3 <zhenguo50@126.com>
2020-06-07 23:26:59 +0800

    @pytest.mark.run(order=x)、@pytest.mark.run(order=1)为testcase目录下所有的测试用例设置执行顺序 2020.06.07

--- testcase/sql2/test_sqlwindow2.py
+++ testcase/sql2/test_sqlwindow2.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pytest
-
 '''
 @Author: LEGEND
 @since: 2020-06-07 18:28:26
-@lastTime: 2020-06-07 23:05:59
+@lastTime: 2020-06-07 23:26:24
 @LastAuthor: Do not edit
 @FilePath: \iPytest\testcase\sql2\test_sqlwindow2.py
 @Description: 
 @version: 
 '''
+
+import pytest
+
 
 @pytest.mark.run(order=1)  # 设置测试用例执行顺序
 def test_sql2():
